60e9a73f535248599e3a70f43534448767d4266b
Urs Reupke <ursreupke@gmail.com>
2014-11-15 22:08:44 +0100

    Cleans up standard selections through maps and arrays

--- random/src/main/java/anathema/android/fashion/GenerateFashion.java
+++ random/src/main/java/anathema/android/fashion/GenerateFashion.java
@@ -3,6 +3,9 @@ package anathema.android.fashion;
 import anathema.android.DiceAndCoins;
 import anathema.android.Flip;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static anathema.android.Flip.Tails;
 
 public class GenerateFashion {
@@ -55,90 +58,61 @@ public class GenerateFashion {
 
   private String rollPrimaryPiece() {
     int roll = rollD20();
-    switch (roll) {
-      case 1:
-        return "{0} wears nothing but {1} bare chest.";
-      case 2:
-        return "{0} keeps {1} body wrapped in bandages.";
-      case 3:
-        return "{0} is protected by smoothly polished tight leathers.";
-      case 4:
-        return "{0} features tight leathers, barbed with metal.";
-      case 5:
-      case 6:
-        return "{0} is dressed in loose monk robes.";
-      case 7:
-        return "{0} sports parade armor with an intricately designed breastplate.";
-      case 8:
-        return "{0} displays parade armor with massive interlocking shoulder plates.";
-      case 9:
-        return "{0} dons a flowing dress.";
-      case 10:
-        return "{0} presents a flowing toga.";
-      case 11:
-        return "{0} wears a worker's leather slacks, covered by a smock.";
-      case 12:
-        return "{0} is dressed in a worker's denim slacks, covered by a smock.";
-      case 13:
-      case 14:
-        String unitSymbol = rollPattern();
-        return "{0} presents a uniform with a {2} " + unitSymbol + " on its high collar.";
-      case 15:
-        return "{0} wrapped {1} body in chain, tight enough to count as clothing.";
-      case 16:
-        return "{0} is covered covered by plates, hiding {1} very humanity.";
-      case 17:
-        return "{0} is decked out in bones and symbols of death.";
-      case 18:
-        return "{0} is wrapped in vines and foliage.";
-      case 19:
-      case 20:
-        return rollSecondaryPiece();
+    if (roll == 13 || roll == 14) {
+      String unitSymbol = rollPattern();
+      return "{0} presents a uniform with a {2} " + unitSymbol + " on its high collar.";
     }
-    throw new IllegalArgumentException("Roll out of range [1,20]");
+    if (roll > 19) {
+      return rollSecondaryPiece();
+    } else {
+      Map<Integer, String> piece = new HashMap<>();
+      piece.put(1, "{0} wears nothing but {1} bare chest.");
+      piece.put(2, "{0} keeps {1} body wrapped in bandages.");
+      piece.put(3, "{0} is protected by smoothly polished tight leathers.");
+      piece.put(4, "{0} features tight leathers, barbed with metal.");
+      piece.put(5, "{0} is dressed in loose monk robes.");
+      piece.put(6, "{0} is dressed in loose monk robes.");
+      piece.put(7, "{0} sports parade armor with an intricately designed breastplate.");
+      piece.put(8, "{0} displays parade armor with massive interlocking shoulder plates.");
+      piece.put(9, "{0} dons a flowing dress.");
+      piece.put(10, "{0} presents a flowing toga.");
+      piece.put(11, "{0} wears a worker's leather slacks, covered by a smock.");
+      piece.put(12, "{0} is dressed in a worker's denim slacks, covered by a smock.");
+      piece.put(15, "{0} wrapped {1} body in chain, tight enough to count as clothing.");
+      piece.put(16, "{0} is covered covered by plates, hiding {1} very humanity.");
+      piece.put(17, "{0} is decked out in bones and symbols of death.");
+      piece.put(18, "{0} is wrapped in vines and foliage.");
+      return piece.get(roll);
+    }
   }
 
   private String rollSecondaryPiece() {
     int roll = rollD20();
-    switch (roll) {
-      case 1:
-      case 2:
-        return "{0} wears a kilt instead of trousers or more traditional armor.";
-      case 3:
-        return "{1} arms are protected by leather wraps.";
-      case 4:
-        return "{1} legs are covered by leather wraps.";
-      case 5:
-        return "Tight martial arts bands cover {1} extremities.";
-      case 6:
-        return "Wraps of cloth, oft sported by martial artists, hide {1} arms and legs.";
-      case 7:
-      case 8:
-        return "Pouches and bags are sewn into every piece of {1} outfit. Who knows what {0} is hiding?";
-      case 9:
-        return "{1} belly is hemmed in by a cummerbund.";
-      case 10:
-        return "{1} midriff is tied with a heavy belt.";
-      case 11:
-        return "A flowing cape accentuates {1} every gesture.";
-      case 12:
-        return "A trench coat hides most of it, though.";
-      case 13:
-        return "{1} shoulder cape adds to {1} well-traveled look.";
-      case 14:
-        return "Whatever else {0} might wear is hidden by a poncho.";
-      case 15:
-      case 16:
-        String pattern = rollPatternAdjective();
-        return pattern + " symbols give {1} appearance a spiritual air.";
-      case 17:
-      case 18:
-        return "Almost as prominent is {1} massive utility vest, stuffed with tools.";
-      case 19:
-      case 20:
-        return rollPrimaryPiece();
+    if (roll == 15 || roll == 16) {
+      String pattern = rollPatternAdjective();
+      return pattern + " symbols give {1} appearance a spiritual air.";
     }
-    throw new IllegalArgumentException("Roll out of range [1,20]");
+    if (roll > 19) {
+      return rollPrimaryPiece();
+    }
+    Map<Integer, String> piece = new HashMap<>();
+    piece.put(1, "{0} wears a kilt instead of more traditional armor.");
+    piece.put(2, "{0} wears a kilt instead of trousers.");
+    piece.put(3, "{1} arms are protected by leather wraps.");
+    piece.put(4, "{1} legs are covered by leather wraps.");
+    piece.put(5, "Tight bands cover {1} extremities, giving {1} a martial artist's look.");
+    piece.put(6, "Wraps of cloth, oft sported by martial artists, hide {1} arms and legs.");
+    piece.put(7, "Pouches and bags are sewn into every piece of {1} outfit. Who knows what {0} is hiding?");
+    piece.put(8, "Pouches and bags are sewn into every piece of {1} outfit. Who knows what {0} is hiding?");
+    piece.put(9, "{1} belly is hemmed in by a cummerbund.");
+    piece.put(10, "{1} midriff is tied with a heavy belt.");
+    piece.put(11, "A flowing cape accentuates {1} every gesture.");
+    piece.put(12, "A trench coat hides most of it, though.");
+    piece.put(13, "{1} shoulder cape adds to {1} well-traveled look.");
+    piece.put(14, "Whatever else {0} might wear is hidden by a poncho.");
+    piece.put(17, "Almost as prominent is {1} massive utility vest, stuffed with tools.");
+    piece.put(18, "Almost as prominent is {1} massive utility vest, stuffed with tools.");
+    return piece.get(roll);
   }
 
   private String rollPattern() {
@@ -218,56 +192,14 @@ public class GenerateFashion {
 
   private String rollPrismaticColor() {
     int roll = rollD10();
-    switch (roll) {
-      case 1:
-        return "red";
-      case 2:
-        return "orange";
-      case 3:
-        return "yellow";
-      case 4:
-        return "green";
-      case 5:
-        return "blue";
-      case 6:
-        return "indigo";
-      case 7:
-        return "violet";
-      case 8:
-        return "black";
-      case 9:
-        return "white";
-      case 10:
-        return "grey";
-    }
-    throw new IllegalArgumentException("Roll out of range [1,10]");
+    String[] colors = new String[]{"red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "white", "grey"};
+    return colors[roll - 1];
   }
 
   private String rollMaterialColor() {
     int roll = rollD10();
-    switch (roll) {
-      case 1:
-        return "ruby";
-      case 2:
-        return "amber";
-      case 3:
-        return "orichalcum";
-      case 4:
-        return "emerald";
-      case 5:
-        return "starmetal";
-      case 6:
-        return "sapphire";
-      case 7:
-        return "amethyst";
-      case 8:
-        return "soulsteel";
-      case 9:
-        return "moonsilver";
-      case 10:
-        return "obsidian";
-    }
-    throw new IllegalArgumentException("Roll out of range [1,10]");
+    String[] colors = new String[]{"ruby", "amber", "orichalcum", "emerald", "starmetal", "sapphire", "amethyst", "soulsteel", "moonsilver", "obsidian"};
+    return colors[roll - 1];
   }
 
   private Flip flipCoin() {
