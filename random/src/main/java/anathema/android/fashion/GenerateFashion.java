bee09c3fe508b20aaacb3378197b6d12246d8fbb
Urs Reupke <ursreupke@gmail.com>
2014-11-16 14:24:40 +0100

    Moves primary and secondary pieces without specialty to JSON files

--- random/src/main/java/anathema/android/fashion/GenerateFashion.java
+++ random/src/main/java/anathema/android/fashion/GenerateFashion.java
@@ -7,9 +7,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static anathema.android.Flip.Tails;
 
 public class GenerateFashion {
@@ -47,61 +44,26 @@ public class GenerateFashion {
 
   private String rollPrimaryPiece() {
     int roll = rollD20();
-    if (roll == 13 || roll == 14) {
+    if (roll >= 19) {
+      return rollSecondaryPiece();
+    }
+    if (roll >= 17) {
       String unitSymbol = rollPattern();
       return "{0} presents a uniform with a {2} " + unitSymbol + " on its high collar.";
     }
-    if (roll >= 19) {
-      return rollSecondaryPiece();
-    } else {
-      Map<Integer, String> piece = new HashMap<>();
-      piece.put(1, "{0} wears nothing but {1} bare chest.");
-      piece.put(2, "{0} keeps {1} body wrapped in bandages.");
-      piece.put(3, "{0} is protected by smoothly polished tight leathers.");
-      piece.put(4, "{0} features tight leathers, barbed with metal.");
-      piece.put(5, "{0} is dressed in loose monk robes.");
-      piece.put(6, "{0} is dressed in loose monk robes.");
-      piece.put(7, "{0} sports parade armor with an intricately designed breastplate.");
-      piece.put(8, "{0} displays parade armor with massive interlocking shoulder plates.");
-      piece.put(9, "{0} dons a flowing dress.");
-      piece.put(10, "{0} presents a flowing toga.");
-      piece.put(11, "{0} wears a worker's leather slacks, covered by a smock.");
-      piece.put(12, "{0} is dressed in a worker's denim slacks, covered by a smock.");
-      piece.put(15, "{0} wrapped {1} body in chain, tight enough to count as clothing.");
-      piece.put(16, "{0} is covered covered by plates, hiding {1} very humanity.");
-      piece.put(17, "{0} is decked out in bones and symbols of death.");
-      piece.put(18, "{0} is wrapped in vines and foliage.");
-      return piece.get(roll);
-    }
+    return pickElementFromJsonArray("primaryPiece.json", "primaryPiece");
   }
 
   private String rollSecondaryPiece() {
     int roll = rollD20();
-    if (roll == 15 || roll == 16) {
-      String pattern = rollPatternAdjective();
-      return pattern + " symbols give {1} appearance a spiritual air.";
-    }
     if (roll >= 19) {
       return rollPrimaryPiece();
     }
-    Map<Integer, String> piece = new HashMap<>();
-    piece.put(1, "{0} wears a kilt instead of more traditional armor.");
-    piece.put(2, "{0} wears a kilt instead of trousers.");
-    piece.put(3, "{1} arms are protected by leather wraps.");
-    piece.put(4, "{1} legs are covered by leather wraps.");
-    piece.put(5, "Tight bands cover {1} extremities, giving {1} a martial artist's look.");
-    piece.put(6, "Wraps of cloth, oft sported by martial artists, hide {1} arms and legs.");
-    piece.put(7, "Pouches and bags are sewn into every piece of {1} outfit. Who knows what {0} is hiding?");
-    piece.put(8, "Pouches and bags are sewn into every piece of {1} outfit. Who knows what {0} is hiding?");
-    piece.put(9, "{1} belly is hemmed in by a cummerbund.");
-    piece.put(10, "{1} midriff is tied with a heavy belt.");
-    piece.put(11, "A flowing cape accentuates {1} every gesture.");
-    piece.put(12, "A trench coat hides most of it, though.");
-    piece.put(13, "{1} shoulder cape adds to {1} well-traveled look.");
-    piece.put(14, "Whatever else {0} might wear is hidden by a poncho.");
-    piece.put(17, "Almost as prominent is {1} massive utility vest, stuffed with tools.");
-    piece.put(18, "Almost as prominent is {1} massive utility vest, stuffed with tools.");
-    return piece.get(roll);
+    if (roll >= 17) {
+      String pattern = rollPatternAdjective();
+      return pattern + " symbols give {1} appearance a spiritual air.";
+    }
+    return pickElementFromJsonArray("secondaryPiece.json", "secondaryPiece");
   }
 
   private String rollPattern() {
@@ -175,7 +137,7 @@ public class GenerateFashion {
   private String pickNameFromJsonArray(String filename, String array) {
     return pickPropertyFromJsonArray(filename, array, "name");
   }
-  
+
   private String pickAttributeFromJsonArray(String filename, String array) {
     return pickPropertyFromJsonArray(filename, array, "attribute");
   }
